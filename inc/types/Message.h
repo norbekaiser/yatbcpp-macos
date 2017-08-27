2bd64658f3d8de33696de3a301bb8fdb55e97be2
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-08-27 09:25:25 +0200

    [CI] Los CI mach was cooles

--- inc/types/Message.h
+++ inc/types/Message.h
@@ -27,6 +27,14 @@
 #include "User.h"
 #include "Chat.h"
 #include "MessageEntity.h"
+#include "Audio.h"
+#include "Document.h"
+#include "Video.h"
+#include "Voice.h"
+#include "VideoNote.h"
+#include "Contact.h"
+#include "Location.h"
+#include "Venue.h"
 
 
 namespace yatbcpp{
@@ -45,15 +53,60 @@ namespace yatbcpp{
 
         void setForward_from_message_id(const std::optional<int> &forward_from_message_id);
 
+        void setForward_signature(const std::optional<std::string> &forward_signature);
+
         void setForward_date(const std::optional<int> &forward_date);
 
-//        void setReply_to_message(const std::optional<Message *> &reply_to_message);
-
         void setEdit_date(const std::optional<int> &edit_date);
+
+        void setAuthor_signature(const std::optional<std::string> &author_signature);
 
         void setText(const std::optional<std::string> &text);
 
         void addEntity(const MessageEntity &messageEntity);
+
+        void setAudio(const std::optional<Audio> &audio);
+
+        void setDocument(const std::optional<Document> &document);
+
+        void addPhoto(const PhotoSize &Photo);
+
+        void setVideo(const std::optional<Video> &video);
+
+        void setVoice(const std::optional<Voice> &voice);
+
+        void setVideo_note(const std::optional<VideoNote> &video_note);
+
+        void addNew_chat_members(const User &new_chat_member);
+
+        void setCaption(const std::optional<std::string> &caption);
+
+        void setContact(const std::optional<Contact> &contact);
+
+        void setLocation(const std::optional<Location> &location);
+
+        void setVenue(const std::optional<Venue> &venue);
+
+        void setNew_chat_member(const std::optional<User> &new_chat_member);
+
+        void setLeft_chat_member(const std::optional<User> &left_chat_member);
+
+        void setNew_chat_title(const std::optional<std::string> &new_chat_title);
+
+        void addNew_chat_photo(const PhotoSize &newchatphoto);
+
+        void setDelete_chat_photo(const std::optional<bool> &delete_chat_photo);
+
+        void setGroup_chat_created(const std::optional<bool> &group_chat_created);
+
+        void setSupergroup_chat_created(const std::optional<bool> &supergroup_chat_created);
+
+        void setChannel_chat_created(const std::optional<bool> &channel_chat_created);
+
+        void setMigrate_to_chat_id(const std::optional<long> &migrate_to_chat_id);
+
+        void setMigrate_from_chat_id(const std::optional<long> &migrate_from_chat_id);
+
 
         int getMessage_id() const;
 
@@ -69,31 +122,139 @@ namespace yatbcpp{
 
         const std::optional<int> &getForward_from_message_id() const;
 
+        const std::optional<std::string> &getForward_signature() const;
+
         const std::optional<int> &getForward_date() const;
 
-//        const std::optional<Message *> &getReply_to_message() const;//auto generated came in here, todo later
-
         const std::optional<int> &getEdit_date() const;
+
+        const std::optional<std::string> &getAuthor_signature() const;
 
         const std::optional<std::string> &getText() const;
 
         const std::vector<MessageEntity> &getEntities() const;
 
+        const std::optional<Audio> &getAudio() const;
+
+        const std::optional<Document> &getDocument() const;
+
+        const std::vector<PhotoSize> &getPhoto() const;
+
+        const std::optional<Video> &getVideo() const;
+
+        const std::optional<Voice> &getVoice() const;
+
+        const std::optional<VideoNote> &getVideo_note() const;
+
+        const std::vector<User> &getNew_chat_members() const;
+
+        const std::optional<std::string> &getCaption() const;
+
+        const std::optional<Contact> &getContact() const;
+
+        const std::optional<Location> &getLocation() const;
+
+        const std::optional<Venue> &getVenue() const;
+
+        const std::optional<User> &getNew_chat_member() const;
+
+        const std::optional<User> &getLeft_chat_member() const;
+
+        const std::optional<std::string> &getNew_chat_title() const;
+
+        const std::vector<PhotoSize> &getNew_chat_photo() const;
+
+        const std::optional<bool> &getDelete_chat_photo() const;
+
+        const std::optional<bool> &getGroup_chat_created() const;
+
+        const std::optional<bool> &getSupergroup_chat_created() const;
+
+        const std::optional<bool> &getChannel_chat_created() const;
+
+        const std::optional<long> &getMigrate_to_chat_id() const;
+
+        const std::optional<long> &getMigrate_from_chat_id() const;
+
+
     private:
-
+        //Unique message identifier inside this chat
         int message_id;
+        //Optional. Sender, empty for messages sent to channels
         std::optional<User> from;//Optional
+        //Date the message was sent in Unix time
         int date;
+        //Conversation the message belongs to
         Chat chat;
+        //Optional. For forwarded messages, sender of the original message
         std::optional<User> forward_from;
+        //Optional. For messages forwarded from channels, information about the original channel
         std::optional<Chat> forward_from_chat;
+        //Optional. For messages forwarded from channels, identifier of the original message in the channel
         std::optional<int> forward_from_message_id;
+        //Optional. For messages forwarded from channels, signature of the post author if present
+        std::optional<std::string> forward_signature;
+        //Optional. For forwarded messages, date the original message was sent in Unix time
         std::optional<int> forward_date;
+//        Optional. For replies, the original message. Note that the Message object in this field will not contain further reply_to_message fields even if it itself is a reply.
 //        std::optional<Message*> reply_to_message;//=NULL;//Optional//sigh auch das noch//naja dann als pointer, ggf adden?
+        //Optional. Date the message was last edited in Unix time
         std::optional<int> edit_date;
+        //Optional. Signature of the post author for messages in channels
+        std::optional<std::string> author_signature;
+        //Optional. For text messages, the actual UTF-8 text of the message, 0-4096 characters.
         std::optional<std::string> text;
-
+        //Optional. For text messages, special entities like usernames, URLs, bot commands, etc. that appear in the text
         std::vector<MessageEntity> entities;
+        //Optional. Message is an audio file, information about the file
+        std::optional<Audio> audio;
+        //Optional. Message is a general file, information about the file
+        std::optional<Document> document;
+        //Optional. Message is a photo, available sizes of the photo
+        std::vector<PhotoSize> photo;
+        //Optional. Message is a video, information about the video
+        std::optional<Video> video;
+        //Optional. Message is a voice message, information about the file
+        std::optional<Voice> voice;
+        //Optional. Message is a video note, information about the video message
+        std::optional<VideoNote> video_note;
+        //Optional. New members that were added to the group or supergroup and information about them (the bot itself may be one of these members)
+        std::vector<User> new_chat_members;
+        //Optional. Caption for the document, photo or video, 0-200 characters
+        std::optional<std::string> caption;
+        //Optional. Message is a shared contact, information about the contact
+        std::optional<Contact> contact;
+        //Optional. Message is a shared location, information about the location
+        std::optional<Location> location;
+        //Optional. Message is a venue, information about the venue
+        std::optional <Venue> venue;
+        //Optional. A new member was added to the group, information about them (this member may be the bot itself)
+        std::optional<User> new_chat_member;
+        //Optional. A member was removed from the group, information about them (this member may be the bot itself)
+        std::optional<User> left_chat_member;
+        //Optional. A chat title was changed to this value
+        std::optional<std::string> new_chat_title;
+        //Optional. A chat photo was change to this value
+        std::vector<PhotoSize> new_chat_photo;
+        //Optional. Service message: the chat photo was deleted
+        std::optional<bool> delete_chat_photo;
+        //Optional. Service message: the group has been created
+        std::optional<bool> group_chat_created;
+        //Optional. Service message: the supergroup has been created. This field can‘t be received in a message coming through updates, because bot can’t be a member of a supergroup when it is created. It can only be found in reply_to_message if someone replies to a very first message in a directly created supergroup.
+        std::optional<bool> supergroup_chat_created;
+        //Optional. Service message: the channel has been created. This field can‘t be received in a message coming through updates, because bot can’t be a member of a channel when it is created. It can only be found in reply_to_message if someone replies to a very first message in a channel.
+        std::optional<bool> channel_chat_created;
+        // 	Optional. The group has been migrated to a supergroup with the specified identifier. This number may be greater than 32 bits and some programming languages may have difficulty/silent defects in interpreting it. But it is smaller than 52 bits, so a signed 64 bit integer or double-precision float type are safe for storing this identifier.
+        std::optional<long> migrate_to_chat_id;
+        //Optional. The supergroup has been migrated from a group with the specified identifier. This number may be greater than 32 bits and some programming languages may have difficulty/silent defects in interpreting it. But it is smaller than 52 bits, so a signed 64 bit integer or double-precision float type are safe for storing this identifier.
+        std::optional<long> migrate_from_chat_id;
+        //Optional. Specified message was pinned. Note that the Message object in this field will not contain further reply_to_message fields even if it is itself a reply.
+//        std::optional<Message*> pinned_message;
+        //todo invoice
+        //todo payment
+
+
+
 
     };
 
--- src/types/Message.cc
+++ src/types/Message.cc
@@ -1,7 +1,8 @@
-//
-// Created by norbert on 18.08.17.
-//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Message Object , represents an incoming message according to bot api definition as close as possible                //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+#include <string>
 #if __has_include(<optional>) 
 #include <optional> 
 #else 
@@ -10,6 +11,17 @@
 #endif 
 #include <json/json.h>
 #include "exceptions/essential_key_missing.h"
+#include "types/User.h"
+#include "types/Chat.h"
+#include "types/MessageEntity.h"
+#include "types/Audio.h"
+#include "types/Document.h"
+#include "types/Video.h"
+#include "types/Voice.h"
+#include "types/VideoNote.h"
+#include "types/Contact.h"
+#include "types/Location.h"
+#include "types/Venue.h"
 #include "types/Message.h"
 
 using namespace yatbcpp;
@@ -66,12 +78,18 @@ Message Message::fromJson(Json::Value Data) {
     if(Data.isMember("forward_from_message_id")){
         ret.setForward_from_message_id(Data["forward_from_message_id"].asInt());
     }
+    if(Data.isMember("forward_signature")){
+        ret.setForward_signature(Data["forward_signature"].asString());
+    }
     if(Data.isMember("forward_date")){
         ret.setForward_date(Data["forward_date"].asInt());
     }
     //todo reply to message
     if(Data.isMember("edit_date")){
         ret.setEdit_date(Data["edit_date"].asInt());
+    }
+    if(Data.isMember("author_signature")){
+        ret.setAuthor_signature(Data["author_signature"].asString());
     }
     if(Data.isMember("text")){
         ret.setText(Data["text"].asString());
@@ -84,6 +102,79 @@ Message Message::fromJson(Json::Value Data) {
             ret.addEntity(messageEntity);
         }
     }
+    if(Data.isMember("audio")){
+        Audio A= Audio::fromJson(Data["audio"]);
+        ret.setAudio(A);
+    }
+    if(Data.isMember("document")){
+        ret.setDocument(Document::fromJson(Data["document"]));
+    }
+    if(Data.isMember("photo")) {
+        for (int i = 0; i < Data["photo"].size(); i++) {
+            ret.addPhoto(PhotoSize::fromJson(Data["photo"][i]));
+        }
+    }
+    //todo missing sticker
+    if(Data.isMember("video")){
+        ret.setVideo(Video::fromJson(Data["video"]));
+    }
+    if(Data.isMember("voice")){
+        ret.setVoice(Voice::fromJson(Data["voice"]));
+    }
+    if(Data.isMember("video_note")){
+        ret.setVideo_note(VideoNote::fromJson(Data["video_note"]));
+    }
+    if(Data.isMember("new_chat_members")) {
+        for (int i = 0; i < Data["new_chat_members"].size(); i++) {
+            ret.addNew_chat_members(User::fromJson(Data["new_chat_members"][i]));
+        }
+    }
+    if(Data.isMember("caption")){
+        ret.setCaption(Data["caption"].asString());
+    }
+    if(Data.isMember("contact")){
+        ret.setContact(Contact::fromJson(Data["contact"]));
+    }
+    if(Data.isMember("location")){
+        ret.setLocation(Location::fromJson(Data["location"]));
+    }
+    if(Data.isMember("venue")){
+        ret.setVenue(Venue::fromJson(Data["venue"]));
+    }
+    if(Data.isMember("new_chat_member")){
+        ret.setNew_chat_member(User::fromJson(Data["new_chat_member"]));
+    }
+    if(Data.isMember("left_chat_member")){
+        ret.setLeft_chat_member(User::fromJson(Data["left_chat_member"]));
+    }
+    if(Data.isMember("new_chat_title")){
+        ret.setNew_chat_title(Data["new_chat_title"].asString());
+    }
+    if(Data.isMember("new_chat_photo")) {
+        for (int i = 0; i < Data["new_chat_photo"].size(); i++) {
+            ret.addNew_chat_photo(PhotoSize::fromJson(Data["new_chat_photo"][i]));
+        }
+    }
+    if(Data.isMember("delete_chat_photo")){
+        ret.setDelete_chat_photo(Data["delete_chat_photo"].asBool());
+    }
+    if(Data.isMember("group_chat_created")){
+        ret.setGroup_chat_created(Data["group_chat_created"].asBool());
+    }
+    if(Data.isMember("supergroup_chat_created")){
+        ret.setSupergroup_chat_created(Data["supergroup_chat_created"].asBool());
+    }
+    if(Data.isMember("channel_chat_created")){
+        ret.setChannel_chat_created(Data["channel_chat_created"].asBool());
+    }
+    if(Data.isMember("migrate_to_chat_id")){
+        ret.setMigrate_to_chat_id(Data["migrate_to_chat_id"].asLargestInt());
+    }
+    if(Data.isMember("migrate_from_chat_id")){//not quite sure if sufficient from spave
+        ret.setMigrate_from_chat_id(Data["migrate_from_chat_id"].asLargestInt());
+    }
+    //pinned message, invoice, sucessfull payment
+
 
     return ret;
 }
@@ -108,6 +199,10 @@ void Message::setForward_from_message_id(const optional<int> &forward_from_messa
     Message::forward_from_message_id = forward_from_message_id;
 }
 
+void Message::setForward_signature(const optional<string> &forward_signature) {
+    Message::forward_signature = forward_signature;
+}
+
 void Message::setForward_date(const optional<int> &forward_date) {
     Message::forward_date = forward_date;
 }
@@ -120,12 +215,104 @@ void Message::setEdit_date(const optional<int> &edit_date) {
     Message::edit_date = edit_date;
 }
 
+void Message::setAuthor_signature(const optional<string> &author_signature) {
+    Message::author_signature = author_signature;
+}
+
 void Message::setText(const optional<string> &text) {
     Message::text = text;
 }
 
 void Message::addEntity(const MessageEntity &messageEntity){
     entities.push_back(messageEntity);
+}
+
+void Message::setAudio(const optional<Audio> &audio) {
+    Message::audio = audio;
+}
+
+void Message::setDocument(const optional<Document> &document) {
+    Message::document = document;
+}
+
+//todo game
+
+void Message::addPhoto(const PhotoSize &Photo) {
+    photo.push_back(Photo);
+}
+
+//todo sticker
+
+void Message::setVideo(const optional<Video> &video) {
+    Message::video = video;
+}
+
+void Message::setVoice(const optional<Voice> &voice) {
+    Message::voice = voice;
+}
+
+void Message::setVideo_note(const optional<VideoNote> &video_note) {
+    Message::video_note = video_note;
+}
+
+void Message::addNew_chat_members(const User &new_chat_member) {
+    new_chat_members.push_back(new_chat_member);
+}
+
+void Message::setCaption(const optional<string> &caption) {
+    Message::caption = caption;
+}
+
+void Message::setContact(const optional<Contact> &contact) {
+    Message::contact = contact;
+}
+
+void Message::setLocation(const optional<Location> &location) {
+    Message::location = location;
+}
+
+void Message::setVenue(const optional<Venue> &venue) {
+    Message::venue = venue;
+}
+
+void Message::setNew_chat_member(const optional<User> &new_chat_member) {
+    Message::new_chat_member = new_chat_member;
+}
+
+void Message::setLeft_chat_member(const optional<User> &left_chat_member) {
+    Message::left_chat_member = left_chat_member;
+}
+
+void Message::setNew_chat_title(const optional<string> &new_chat_title) {
+    Message::new_chat_title = new_chat_title;
+}
+
+void Message::addNew_chat_photo(const PhotoSize &newchatphoto) {
+    new_chat_photo.push_back(newchatphoto);
+}
+
+void Message::setDelete_chat_photo(const optional<bool> &delete_chat_photo) {
+    Message::delete_chat_photo = delete_chat_photo;
+}
+
+void Message::setGroup_chat_created(const optional<bool> &group_chat_created) {
+    Message::group_chat_created = group_chat_created;
+}
+
+void Message::setSupergroup_chat_created(const optional<bool> &supergroup_chat_created) {
+    Message::supergroup_chat_created = supergroup_chat_created;
+}
+
+void Message::setChannel_chat_created(const optional<bool> &channel_chat_created) {
+    Message::channel_chat_created = channel_chat_created;
+}
+
+void Message::setMigrate_to_chat_id(const optional<long> &migrate_to_chat_id) {
+    Message::migrate_to_chat_id = migrate_to_chat_id;
+}
+
+void Message::setMigrate_from_chat_id(const optional<long> &migrate_from_chat_id) {
+    Message::migrate_from_chat_id = migrate_from_chat_id;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -160,6 +347,10 @@ const optional<int> &Message::getForward_from_message_id() const {
     return forward_from_message_id;
 }
 
+const optional<string> &Message::getForward_signature() const {
+    return forward_signature;
+}
+
 const optional<int> &Message::getForward_date() const {
     return forward_date;
 }
@@ -172,6 +363,10 @@ const optional<int> &Message::getEdit_date() const {
     return edit_date;
 }
 
+const optional<string> &Message::getAuthor_signature() const {
+    return author_signature;
+}
+
 const optional<string> &Message::getText() const {
     return text;
 }
@@ -179,6 +374,92 @@ const optional<string> &Message::getText() const {
 const vector<MessageEntity> &Message::getEntities() const {
     return entities;
 }
+
+const optional<Audio> &Message::getAudio() const {
+    return audio;
+}
+
+const optional<Document> &Message::getDocument() const {
+    return document;
+}
+
+const vector<PhotoSize> &Message::getPhoto() const {
+    return photo;
+}
+
+const optional<Video> &Message::getVideo() const {
+    return video;
+}
+
+const optional<Voice> &Message::getVoice() const {
+    return voice;
+}
+
+const optional<VideoNote> &Message::getVideo_note() const {
+    return video_note;
+}
+
+const vector<User> &Message::getNew_chat_members() const {
+    return new_chat_members;
+}
+
+const optional<string> &Message::getCaption() const {
+    return caption;
+}
+
+const optional<Contact> &Message::getContact() const {
+    return contact;
+}
+
+const optional<Location> &Message::getLocation() const {
+    return location;
+}
+
+const optional<Venue> &Message::getVenue() const {
+    return venue;
+}
+
+const optional<User> &Message::getNew_chat_member() const {
+    return new_chat_member;
+}
+
+const optional<User> &Message::getLeft_chat_member() const {
+    return left_chat_member;
+}
+
+const optional<string> &Message::getNew_chat_title() const {
+    return new_chat_title;
+}
+
+const vector<PhotoSize> &Message::getNew_chat_photo() const {
+    return new_chat_photo;
+}
+
+const optional<bool> &Message::getDelete_chat_photo() const {
+    return delete_chat_photo;
+}
+
+const optional<bool> &Message::getGroup_chat_created() const {
+    return group_chat_created;
+}
+
+const optional<bool> &Message::getSupergroup_chat_created() const {
+    return supergroup_chat_created;
+}
+
+const optional<bool> &Message::getChannel_chat_created() const {
+    return channel_chat_created;
+}
+
+const optional<long> &Message::getMigrate_to_chat_id() const {
+    return migrate_to_chat_id;
+}
+
+const optional<long> &Message::getMigrate_from_chat_id() const {
+    return migrate_from_chat_id;
+}
+
+
 
 
 
