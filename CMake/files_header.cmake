908f4b28c652bbff4dc417ce746677a0491865b7
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-09-01 09:21:23 +0200

    [CI] refactoren am ar***, fixed issue with missing refactoration

---
+++ CMake/files_header.cmake
@@ -0,0 +1,15 @@
+########################################################################################################################
+# Telegram Bot Header Files                                                                                            #
+########################################################################################################################
+file(GLOB yatbcppBotHeadersList
+        "inc/bot/*.h"
+        )
+file(GLOB yatbcppMethodsHeadersList
+        "inc/methods/*.h"
+        )
+file(GLOB yatbcppTypesHeadersList
+        "inc/types/*.h"
+        )
+file(GLOB yatbcppExceptionsHeadersList
+        "inc/exceptions/*.h"
+        )
