3ddcbed49f60be9c055c8587be6ed2b79ea134ec
Deploy Runner <ci-yatbcpp-deploy@norbert-ruehl.de>
2017-11-11 13:10:18 +0100

    [CI] CI y u let this pass

--- inc/types/chatmember_status.h
+++ inc/types/chatmember_status.h
@@ -9,8 +9,8 @@
 //        2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 //    
 //        3. This notice may not be removed or altered from any source distribution.
-#ifndef YATBCPP_CHAT_TYPE_H
-#define YATBCPP_CHAT_TYPE_H
+#ifndef YATBCPP_CHATMEMBER_STATUS_H
+#define YATBCPP_CHATMEMBER_STATUS_H
 
 namespace yatbcpp{
 
